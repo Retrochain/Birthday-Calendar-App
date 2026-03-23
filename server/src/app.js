ab39a4c908709fad1fe920906f8e6c1ab48d71d0
Retrochain <akshaansingh.2018@gmail.com>
2026-03-23 11:45:08 -0500

    Set up a new router middleware for birthday endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,18 @@
 import express from "express";
 import cors from "cors";
 
+// Import the birthdays router, which contains routes for managing birthday data
+import birthdaysRouter from '../routes/birthdays.js';
+
 // Create an instance of the Express application
 const app = express();
 
 // Use CORS middleware to allow cross-origin requests and JSON middleware to parse incoming JSON request bodies
 app.use(cors());
 app.use(express.json());
+
+// Use the birthdays router for any routes that start with /birthdays
+app.use('/birthdays', birthdaysRouter);
 
 // Define a simple route for the root URL that responds with "Hello, World!" when accessed
 app.get("/", (req, res) => {
