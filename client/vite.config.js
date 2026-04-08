559f0e2e64c9037265c269cf9337d977712cae0a
Retrochain <akshaansingh.2018@gmail.com>
2026-04-07 22:01:42 -0500

    Updated some leftover code from last merge

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": "http://localhost:5000",
